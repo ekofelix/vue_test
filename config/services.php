bbd8f606ab91eb207616e14951c2b8be93df5f84
ekofelix <adioswokeh@gmail.com>
2022-06-27 16:38:50 +0700

    day 1

---
+++ config/services.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'google' => [
+
+        'client_id' => '377117841913-sk7k4h1l8q3lcbo46qvesuhg897ehdl7.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect' => 'http://127.0.0.1:8000/callback/google',
+    ],
+
+];
